5ccecea6737ff0ffa89489db8e4d0aa8e0b659a3
zakjakub <mail@jakubzak.eu>
2019-06-01 17:20:13 +0200

    Some changes.

--- Form/EventParticipant/EventParticipantNoteType.php
+++ Form/EventParticipant/EventParticipantNoteType.php
@@ -3,15 +3,11 @@
 namespace Zakjakub\OswisAddressBookBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\Exception\AccessException;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Zakjakub\OswisAddressBookBundle\Entity\ContactNote;
-use function assert;
+use Zakjakub\OswisCalendarBundle\Entity\EventParticipant\EventParticipantNote;
 
 class EventParticipantNoteType extends AbstractType
 {
@@ -19,32 +15,14 @@ class EventParticipantNoteType extends AbstractType
     {
         $builder
             ->add(
-                'content',
+                'textValue',
                 TextType::class,
                 array(
                     'label'    => 'Poznámka',
-                    'required' => true,
+                    'required' => false,
                     'attr'     => ['placeholder' => false],
                 )
             );
-
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            static function (FormEvent $event) {
-                $contactNote = $event->getData();
-                assert($contactNote instanceof ContactNote);
-                $form = $event->getForm();
-                $form->add(
-                    'content',
-                    TextareaType::class,
-                    array(
-                        'label'    => $contactNote->isPublic() ? 'Poznámka pro účastníky' : 'Poznámka pro pořadatele',
-                        'required' => false,
-                        'help'     => $contactNote->isPublic() ? 'Poznámka, která bude zveřejněná účastníkům.' : 'Neveřejná poznámka, určená pouze pro pořadatele.',
-                    )
-                );
-            }
-        );
     }
 
 
@@ -57,7 +35,7 @@ class EventParticipantNoteType extends AbstractType
     {
         $resolver->setDefaults(
             array(
-                'data_class' => ContactNote::class,
+                'data_class' => EventParticipantNote::class,
 //                'attr' => ['class' => 'col-md-6'],
             )
         );
@@ -65,6 +43,6 @@ class EventParticipantNoteType extends AbstractType
 
     final public function getName(): string
     {
-        return 'address_book_contact_note';
+        return 'calendar_event_participant_note';
     }
 }
