1c88cbf6391805a4353733cc95d6ba1e6fd8e5e3
zakjakub <mail@jakubzak.eu>
2020-06-06 11:37:55 +0200

    Changes...

--- Service/ParticipantFlagTypeService.php
+++ Service/ParticipantFlagTypeService.php
@@ -6,37 +6,30 @@ use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use OswisOrg\OswisCalendarBundle\Entity\EventAttendeeFlag;
 use OswisOrg\OswisCalendarBundle\Entity\Participant\ParticipantFlagType;
-use OswisOrg\OswisCoreBundle\Entity\NonPersistent\Nameable;
 use Psr\Log\LoggerInterface;
 
 class ParticipantFlagTypeService
 {
     protected EntityManagerInterface $em;
 
-    protected ?LoggerInterface $logger;
+    protected LoggerInterface $logger;
 
-    public function __construct(EntityManagerInterface $em, ?LoggerInterface $logger = null)
+    public function __construct(EntityManagerInterface $em, LoggerInterface $logger)
     {
         $this->em = $em;
         $this->logger = $logger;
     }
 
-    final public function create(
-        ?Nameable $nameable = null,
-        ?string $type = null,
-        ?int $minFlagsAllowed = null,
-        ?int $maxFlagsAllowed = null
-    ): ?ParticipantFlagType {
+    final public function create(ParticipantFlagType $flagType): ?ParticipantFlagType
+    {
         try {
-            $entity = new ParticipantFlagType($nameable, $type, $minFlagsAllowed, $maxFlagsAllowed);
-            $this->em->persist($entity);
+            $this->em->persist($flagType);
             $this->em->flush();
-            $infoMessage = 'CREATE: Created event participant flag type (by service): '.$entity->getId().' '.$entity->getName().'.';
-            $this->logger ? $this->logger->info($infoMessage) : null;
+            $this->logger->info('CREATE: Created participant flag type (by service): '.$flagType->getId().' '.$flagType->getShortName().'.');
 
-            return $entity;
+            return $flagType;
         } catch (Exception $e) {
-            $this->logger ? $this->logger->info('ERROR: Event event participant flag type not created (by service): '.$e->getMessage()) : null;
+            $this->logger->info('ERROR: Event participant flag type not created (by service): '.$e->getMessage());
 
             return null;
         }
